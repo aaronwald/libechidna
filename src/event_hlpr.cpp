6ce4adda69ce234c2fc68c4b3d53499080ca5576
Aaron Wald <aaronwald@gmail.com>
2022-11-24 23:17:43 +0000

    asdfadsfasdf

--- src/event_hlpr.cpp
+++ src/event_hlpr.cpp
@@ -233,15 +233,9 @@ int IOURingHelper::SubmitReadv(coypu_io_uring &ring, int file_fd, struct iovec *
 }
 
 // io_vecs cant go away
-int IOURingHelper::SubmitWritev(coypu_io_uring &ring, int file_fd, struct iovec *iovecs)
+int IOURingHelper::SubmitWritev(coypu_io_uring &ring, int file_fd, struct iovec *iovecs, uint32_t len, void *userdata)
 {
-  (void)ring;
-  (void)file_fd;
-  (void)iovecs;
-  // (void)len;
-  // (void)userdata;
-  return 0;
-  // return IOURingHelper::Submit(ring, file_fd, IORING_OP_WRITEV, iovecs, len, userdata);
+  return IOURingHelper::Submit(ring, file_fd, IORING_OP_WRITEV, iovecs, len, userdata);
 }
 
 void IOURingHelper::ReadCompletion(coypu_io_uring &ring)
