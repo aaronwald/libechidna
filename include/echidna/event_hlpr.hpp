a4c9e74f837f474c7c20bb181b383c2d70d5ecba
Aaron Wald <aaronwald@gmail.com>
2023-05-13 12:19:24 +0000

    add recv

--- include/echidna/event_hlpr.hpp
+++ include/echidna/event_hlpr.hpp
@@ -141,12 +141,24 @@ namespace coypu::event
 			return add_to_sqe(ring, &sqe);
 		}
 
-		static inline int SubmitsEND(coypu_io_uring &ring, int file_fd, char *buf, uint32_t len, uint64_t userdata)
+		static inline int SubmitSend(coypu_io_uring &ring, int file_fd, char *buf, uint32_t len, uint64_t userdata)
 		{
 			struct io_uring_sqe sqe;
 			::memset(&sqe, 0, sizeof(sqe));
 			sqe.fd = file_fd;
 			sqe.opcode = IORING_OP_SEND; // https://manpages.debian.org/unstable/liburing-dev/io_uring_enter.2.en.html
+			sqe.addr = (unsigned long long)buf;
+			sqe.len = len;
+			sqe.user_data = (unsigned long long)userdata; // user data
+			return add_to_sqe(ring, &sqe);
+		}
+
+		static inline int SubmitRecv(coypu_io_uring &ring, int file_fd, char *buf, uint32_t len, uint64_t userdata)
+		{
+			struct io_uring_sqe sqe;
+			::memset(&sqe, 0, sizeof(sqe));
+			sqe.fd = file_fd;
+			sqe.opcode = IORING_OP_RECV; // https://manpages.debian.org/unstable/liburing-dev/io_uring_enter.2.en.html
 			sqe.addr = (unsigned long long)buf;
 			sqe.len = len;
 			sqe.user_data = (unsigned long long)userdata; // user data
