4451ba1f837deca20ab0febe868d4d2566fd1abb
Aaron Wald <aaronwald@gmail.com>
2025-01-23 20:47:30 +0000

    add warning

--- include/echidna/log.hpp
+++ include/echidna/log.hpp
@@ -3,5 +3,5 @@
 
 #define ECHIDNA_LOG_DEBUG(logger, fmt, ...) LOG_DEBUG(logger, fmt, ##__VA_ARGS__)
 #define ECHIDNA_LOG_INFO(logger, fmt, ...) LOG_INFO(logger, fmt, ##__VA_ARGS__)
-#define ECHIDNA_LOG_WARNING(logger, fmt, ...) LOG_WARN(logger, fmt, ##__VA_ARGS__)
+#define ECHIDNA_LOG_WARNING(logger, fmt, ...) LOG_WARNING(logger, fmt, ##__VA_ARGS__)
 #define ECHIDNA_LOG_ERROR(logger, fmt, ...) LOG_ERROR(logger, fmt, ##__VA_ARGS__)
